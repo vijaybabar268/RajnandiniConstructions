e1cc46cb1854ee30359d6eff2e24422195712d71
Vijaybabar <babarvm@gmail.com>
2026-04-11 12:29:08 +0530

    Created required components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { SiteComponent } from './admin/site/site.component';
+import { AccountComponent } from './admin/account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AccountComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'sites', component: SiteComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { SiteComponent } from './admin/site/site.component';
+import { AccountComponent } from './admin/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    SiteComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
